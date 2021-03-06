85e05ff18f12ba96199a84437ae7e8e6c1154259
ayeeshaEP <ellisayeesha@gmail.com>
2021-03-06 11:28:21 -0800

    more on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
 import Layout from './layouts/Layout';
 import Login from './screens/Login';
+import { loginUser, registerUser } from './services/auth';
+import Register from './screens/Register';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  const handleLogin = async (formData) => {
+    const currentUser = await loginUser(formData);
+    setCurrentUser(currentUser);
+    history.push('/');
+  }
+  const handleRegister = async (formData) => {
+    const currentUser = await registerUser(formData);
+    setCurrentUser(currentUser);
+    history.push('/');
+  }
+
   return (
     <Layout>
     <Switch>
       <Route path='/login'>
-        <Login/>
+        <Login handleLogin={handleLogin}/>
       </Route>
       <Route path='/register'>
-
+      <Register handleRegister={handleRegister}/>
       </Route>
       <Route path='/'>
        
@@ -21,4 +38,5 @@ function App() {
     </Layout>
   );
 }
+
 export default App;
